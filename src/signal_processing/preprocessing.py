f5e53b54429c33c48c153f0cbf9edb4f8a59b178
alx <agarkov.aleksei1@yandex.ru>
2026-06-25 15:39:20 +0300

    Update noise threshold processing

--- src/signal_processing/preprocessing.py
+++ src/signal_processing/preprocessing.py
@@ -19,10 +19,8 @@ def filter_signal_end_to_window(arr, window_size):
 
 @transform
 def calc_base_signal_components(row,
-                                signal_level_threshold_at_end=0.1,
-                                noise_threshold: int = 24,
                                 start_idx: int = 140,
-                                end_offset=20):
+                                end_offset=5):
     arr = row['data_array']
     if row.name == 1929:
         arr = row['data_array'][:300]
@@ -31,8 +29,11 @@ def calc_base_signal_components(row,
 
     peak_idx = len(arr) - 1 - np.argmax(arr[::-1])
     amplitude = arr[peak_idx]
+    noise = arr[:start_idx]
+    noise_std = np.std(noise)
+    noise_threshold = noise_std * 3
 
-    signal_indices_in_data_array = np.where(arr > max(signal_level_threshold_at_end * amplitude, noise_threshold))[0]
+    signal_indices_in_data_array = np.where(arr > noise_threshold * 3)[0]
     signal_indices_in_data_array = filter_signal_end_to_window(signal_indices_in_data_array, end_offset)
 
     end = min(len(arr), signal_indices_in_data_array[-1] + end_offset)
@@ -41,13 +42,15 @@ def calc_base_signal_components(row,
     signal_array = arr[signal_indices_in_data_array]
     peak_idx_in_signal = len(signal_array) - 1 - np.argmax(signal_array[::-1])
 
-    return {'signal_start': signal_indices_in_data_array[0],
-            'signal_end': signal_indices_in_data_array[-1],
-            'signal_indices_in_data_array': signal_indices_in_data_array,
+    return {'signal_indices_in_data_array': signal_indices_in_data_array,
             'signal_array': signal_array,
             'signal_own_indices': (np.arange(len(signal_array))),
             'peak_idx': peak_idx,
-            'peak_idx_in_signal': peak_idx_in_signal,
+            'signal_start': signal_indices_in_data_array[0],
             'amplitude': amplitude,
+            'peak_idx_in_signal': peak_idx_in_signal,
+            'noise_std': noise_std,
+            'noise_threshold': noise_threshold,
+            'signal_end': signal_indices_in_data_array[-1],
             'signal_area': (np.trapezoid(signal_array)),
             'signal_duration': (len(signal_array))}
